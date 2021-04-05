61c5d902403555e8845ce34ac606e4a27f834ede
Ana Graca <patricia.d.s.graca@gmail.com>
2021-04-05 11:07:09 +0100

    learning about different forms

--- tests/15_form.php
+++ tests/15_form.php
@@ -1,5 +1,5 @@
 <?php
-
+//only print the array if the form was submited
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
   var_dump($_POST);
 }
@@ -15,10 +15,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
 
 <form method="post">
-
-    <input name="search">
-    <input name="password" type="password">
-
+    <!-- name atribute allows to submit to the server and not to itself -->
+    <div>
+      <input name="search" value="username">
+      <input name="password" type="password">
+    </div>
+    <!--input types: https://devdocs.io/html/element/input -->
+    <div>
+      date : <input name="date" type="date">
+    </div>
+    <!-- you can see the value on page source code! DO NOT USE FOR SENSITIVE DATA-->
+    <div>
+      hidden : <input name="hidden" type="hidden" value="1234">
+    </div>
+    <div>
+      textarea: <textarea name="content" rows="7" cols="50">Hello</textarea>
+    </div>
     <button>Send</button>
 
 </form>
---
+++ tests/16_form_menu.php
@@ -0,0 +1,42 @@
+<?php
+//only print the array if the form was submited
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  var_dump($_POST);
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forms</title>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<form method="post">
+    <select name="marque[]" multiple>
+      <option value="bmw"> BMW </option>
+      <option value="fmc"> Ford </option>
+      <option value="saab"> Sabb </option>
+    </select>
+
+    <select name="country">
+      <optgroup label="Europe">
+          <option value="germany"> Germany </option>
+          <option value="france"> France </option>
+          <option value="uk" selected > United Kingdom </option>
+      </optgroup>
+      <optgroup label="America">
+          <option value="brazil"> Brazil </option>
+          <option value="canada"> Canada </option>
+          <option value="usa"> United States </option>
+      </optgroup>
+    </select>
+
+    <button>Send</button>
+
+</form>
+
+</body>
+</html>
